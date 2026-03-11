2bee49b5b767471abcbdea8ba9e485e9db17bdca
Anvi Mehta <am5765@srmist.edu.in>
2026-03-11 20:35:29 +0530

    PrintBanner code

---
+++ eclipse-workspace/PrintBanner/src/com/oops/PrintBanner.java
@@ -0,0 +1,12 @@
+package com.oops;
+public class PrintBanner{
+	public static void main(String[] args) {
+	            System.out.println(" ***     ***    *****    ***** ");
+		        System.out.println("*   *   *   *   *    *   *    ");
+		        System.out.println("*   *   *   *   *    *   *    ");
+		        System.out.println("*   *   *   *   *****    ***** ");
+		        System.out.println("*   *   *   *   *            *");
+		        System.out.println("*   *   *   *   *            *");
+		        System.out.println(" ***     ***    *        ***** ");
+	}  
+} 
